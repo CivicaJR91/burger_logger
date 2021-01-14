e955c8667b25f4cb100276fccec65dec763cca9b
Janliz Suarez <jsuarezrosado@gmail.com>
2021-01-13 20:40:48 -0600

    remove mysql pass

--- config/connection.js
+++ config/connection.js
@@ -5,7 +5,7 @@ const connection = mysql.createConnection({
     host:"localhost",
     port: 3306,
     user: "root",
-    password:"MYSQL01233" ,//Add MySQL Password
+    password:"" ,//Add MySQL Password
     database:"burgers_db"
 });
 
