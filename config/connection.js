ef7a3a7d9665ca2ae68ddefcd96503a33e4032b8
Janliz Suarez <jsuarezrosado@gmail.com>
2021-01-16 21:17:54 -0600

    connected to horoku

--- config/connection.js
+++ config/connection.js
@@ -1,15 +1,15 @@
 //Setting up MYSQL Connection
-const mysql = require("mysql");
+const mysql = require('mysql');
 
-const connection = mysql.createConnection({
-    host:"localhost",
-    port: 3306,
-    user: "root",
-    password:"" ,//Add MySQL Password
-    database:"burgers_db"
-});
+// const connection = mysql.createConnection({
+//     host:"localhost",
+//     port: 3306,
+//     user: "root",
+//     password:"" ,//Add MySQL Password
+//     database:"burgers_db"
+// });
 
-//Making Connection
+// // Making Connection
 // connection.connect(function(err){
 //     if (err){
 //         console.error("Error" + err.stack);
@@ -23,12 +23,12 @@ const connection = mysql.createConnection({
 var herukuConnect;
 
 if(process.env.JAWSDB_URL){
-    herukuConnect= mysql.createConnection(process.env.JAWSDB_URL);
+    herukuConnect = mysql.createConnection(process.env.JAWSDB_URL);
 } else{
     herukuConnect = mysql.createConnection({
     host:"j21q532mu148i8ms.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
     user: "xnq1e652cg4l7qcy",
-    password:"xzzjvw3oibroxnyn" , // add heruko pass
+    password:"xzzjvw3oibroxnyn", // add heruko pass
     database:"wedrc380ht2h0j6l" // add heruko database
     });
 };
@@ -38,4 +38,4 @@ herukuConnect.connect();
 
 //Export connection to ORM
 
-module.exports = connection;
+module.exports = herukuConnect;
